12463e6dd548ee44363d45124e0ca5a71cdcb3d3
Nico Arato <nico.arato@gmail.com>
2021-08-22 21:43:55 -0300

    Pruebas unitarias con counterApp

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
 
     const [counter, setCounter] = useState(value);
 
@@ -20,13 +20,12 @@ const CounterApp = ({ value }) => {
     return (
         <>
             <h1> Counter App </h1>
-            <p> {counter} </p>
-            <button onClick={handleAdd}>+1</button>
-            <button onClick={handleReset}>Reset</button>
-            <button onClick={handleSubstract}>-1</button>
+            <h2> {counter} </h2>
+            <button onClick={handleAdd} > +1 </ button>
+            <button onClick={handleReset} > Reset </ button>
+            <button onClick={handleSubstract} > -1 </ button>
         </>
     )
-
 };
 
 CounterApp.propTypes = {
---
+++ src/tests/CounterApp.test.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { shallow } from "enzyme";
+import CounterApp from "../CounterApp";
+
+describe('Pruebas en el <CounterApp />', () => {
+
+
+    let wrapper = shallow(< CounterApp />);
+
+    beforeEach(() => {
+        wrapper = shallow(< CounterApp />);
+    });
+
+    test('Debe mostrar el <CounterApp /> correctamente', () => {
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe mostrar el valor por defecto de 100', () => {
+
+        const wrapper = shallow(< CounterApp value={100} />);
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('100');
+    });
+
+    test('Debe incrementar con el boton +1', () => {
+
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('11');
+
+    });
+
+    test('Debe decrementar con el boton -1', () => {
+
+        wrapper.find('button').at(2).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('9');
+
+    });
+
+    test('Debe colocar el valor por defecto con el btn Reset', () => {
+
+        const wrapper = shallow(< CounterApp value={105} />);
+
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(0).simulate('click');
+
+        wrapper.find('button').at(1).simulate('click');
+
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('105');
+
+
+
+
+    })
+
+
+
+
+})
